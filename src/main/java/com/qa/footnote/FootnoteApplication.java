4b5a5ba139db132b1539d7b5b9e269c91cebe1cf
Basia Adamiec <badamiec3@live.com>
2020-10-14 11:02:14 +0100

    Initial commit

---
+++ src/main/java/com/qa/footnote/FootnoteApplication.java
@@ -0,0 +1,13 @@
+package com.qa.footnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootnoteApplication.class, args);
+	}
+
+}
