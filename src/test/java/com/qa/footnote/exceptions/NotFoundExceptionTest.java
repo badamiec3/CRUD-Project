be3c9f40dc9833bf4baefe277bad0d8eeb17caac
Basia Adamiec <badamiec3@live.com>
2020-10-17 18:31:15 +0100

    Reverted back to h2 to allow boolean

---
+++ src/test/java/com/qa/footnote/exceptions/NotFoundExceptionTest.java
@@ -0,0 +1,10 @@
+package com.qa.footnote.exceptions;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
+public class NotFoundExceptionTest {
+
+}
